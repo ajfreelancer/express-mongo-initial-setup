5fce97a733f66120536d389289e9491b3f7caed1
Abdul Jabbar <41137069+ajfreelancer@users.noreply.github.com>
2022-03-15 08:28:32 +0500

    made api simpler to check if it is working under vercel capped time limit

--- api/goals.js
+++ api/goals.js
@@ -1,9 +1,10 @@
-const express = require('express');	
+const express = require("express");
 const router = express.Router();
-const Goals = require('../backend/models/Goals');
+// const Goals = require("../backend/models/Goals");
 
 router.get("/api/goals", (req, res) => {
-    const goals = Goals.find().then((goals) => res.json(goals));
-  });
+  // const goals = Goals.find().then((goals) => res.json(goals));
+  res.json({"message": "Hello World"});
+});
 
 module.exports = router;
