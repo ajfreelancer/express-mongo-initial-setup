29a2275c157c649d53409bfcf8d98f6d99403a5e
Abdul Jabbar <41137069+ajfreelancer@users.noreply.github.com>
2022-03-15 08:53:40 +0500

    added a log statement in api/goals

--- api/goals.js
+++ api/goals.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Goals = require("../backend/models/Goals");
 
 router.get("/api/goals", (req, res) => {
+  console.log("get request for all goals");
   const goals = Goals.find().then((goals) => res.json(goals));
 });
 
