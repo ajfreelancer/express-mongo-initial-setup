30b0ddd51c40b3d6f1f7be6df2ff38e2f92b9405
Abdul Jabbar <41137069+ajfreelancer@users.noreply.github.com>
2022-03-15 09:44:26 +0500

    caught error on adding posts

--- backend/routes/goals.js
+++ backend/routes/goals.js
@@ -1,19 +1,20 @@
-const express = require('express');	
+const express = require("express");
 const router = express.Router();
-const Goals = require('../models/Goals');
+const Goals = require("../models/Goals");
 
-router.post('/goals', (req, res) => {
+router.post("/goals", (req, res) => {
+  console.table(req.body);
 
-    console.table(req.body);
+  const newGoal = new Goals({
+    title: req.body.title,
+    description: req.body.description,
+    completed: req.body.completed,
+    createdAt: req.body.createdAt,
+  });
 
-    const newGoal = new Goals({
-        title: req.body.title,
-        description: req.body.description,
-        completed: req.body.completed,
-        createdAt: req.body.createdAt
-    });
-
-    newGoal.save().then(goal => res.json(goal));
-})
+  newGoal.save()
+    .then((goal) => res.json(goal))
+    .catch((err) => console.log(err));
+});
 
 module.exports = router;
