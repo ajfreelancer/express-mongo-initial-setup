a6d34440860f4c250716162b48d6368b156cff09
Abdul Jabbar <41137069+ajfreelancer@users.noreply.github.com>
2022-03-15 08:35:59 +0500

    reverted api to original condition

--- api/goals.js
+++ api/goals.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const router = express.Router();
-// const Goals = require("../backend/models/Goals");
+const Goals = require("../backend/models/Goals");
 
 router.get("/api/goals", (req, res) => {
-  // const goals = Goals.find().then((goals) => res.json(goals));
-  res.json({"message": "Hello World"});
+  const goals = Goals.find().then((goals) => res.json(goals));
 });
 
 module.exports = router;
